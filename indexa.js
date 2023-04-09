bbd2285e18ae45e737ec3a82ee418125cdcfff04
Joe <joedsonfera@hotmail.com>
2023-04-09 16:37:08 -0300

    third commit

---
+++ indexa.js
@@ -0,0 +1,17 @@
+
+let name = "Joe";
+let age = 31;
+
+let person = {
+    name: "Joe",
+    age: 30
+};
+
+// Dot Notation
+person.name = "John";
+
+//Bracket Notation
+let selection = "name";
+person[selection] = "Ana";
+
+console.log(person.name);
