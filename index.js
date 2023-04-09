d05590f04c9d5788aba959578dd8c9b3956cfd2f
Joe <joedsonfera@hotmail.com>
2023-04-09 16:08:31 -0300

    js-basics-with-hiei

---
+++ index.js
@@ -0,0 +1,26 @@
+// This is my second JavaScript code!"
+console.log("Hello World!");
+
+let name = "Joe";
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be a meaninful
+// Cannot start witha a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = "Ana";
+let lastName = "Clara";
+
+let interestRate = 0.3;
+interestRate - 1;
+console.log(interestRate);
+
+let Name = "Joe"; // String Literal
+let age = 31; // Number Literal
+let isApproved = true; // Boolean Literal
+let firstnome = undefined;
+let selectedColor = null;
+console.log(name)
+
